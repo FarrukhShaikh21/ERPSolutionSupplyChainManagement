e2443f2509e1008eb6f7b147d325af149869de04
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-15 15:40:06 +0500

    adding taskflow and template

--- ERPSolSCMViewController/src/erpsolscm/erpsolscmview/backing/ERPSolSCMMain.java
+++ ERPSolSCMViewController/src/erpsolscm/erpsolscmview/backing/ERPSolSCMMain.java
@@ -2,20 +2,17 @@ package erpsolscm.erpsolscmview.backing;
 
 import oracle.adf.view.rich.component.rich.RichDocument;
 import oracle.adf.view.rich.component.rich.RichForm;
-import oracle.adf.view.rich.component.rich.fragment.RichPageTemplate;
+import oracle.adf.view.rich.component.rich.layout.RichPanelHeader;
+import oracle.adf.view.rich.component.rich.layout.RichToolbar;
+import oracle.adf.view.rich.component.rich.nav.RichButton;
 
 public class ERPSolSCMMain {
-    private RichPageTemplate pt1;
     private RichForm f1;
     private RichDocument d1;
+    private RichPanelHeader ph1;
+    private RichToolbar t1;
+    private RichButton b1;
 
-    public void setPt1(RichPageTemplate pt1) {
-        this.pt1 = pt1;
-    }
-
-    public RichPageTemplate getPt1() {
-        return pt1;
-    }
 
     public void setF1(RichForm f1) {
         this.f1 = f1;
@@ -31,5 +28,29 @@ public class ERPSolSCMMain {
 
     public RichDocument getD1() {
         return d1;
+    }
+
+    public void setPh1(RichPanelHeader ph1) {
+        this.ph1 = ph1;
+    }
+
+    public RichPanelHeader getPh1() {
+        return ph1;
+    }
+
+    public void setT1(RichToolbar t1) {
+        this.t1 = t1;
+    }
+
+    public RichToolbar getT1() {
+        return t1;
+    }
+
+    public void setB1(RichButton b1) {
+        this.b1 = b1;
+    }
+
+    public RichButton getB1() {
+        return b1;
     }
 }
