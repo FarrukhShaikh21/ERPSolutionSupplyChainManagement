7f99d23c98e515db02a82d561e265857688dbee8
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-15 14:18:16 +0500

    ADDING TEMPLATE

---
+++ ERPSolSCMViewController/src/erpsolscm/erpsolscmview/backing/ERPSolSCMMain.java
@@ -0,0 +1,35 @@
+package erpsolscm.erpsolscmview.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.fragment.RichPageTemplate;
+
+public class ERPSolSCMMain {
+    private RichPageTemplate pt1;
+    private RichForm f1;
+    private RichDocument d1;
+
+    public void setPt1(RichPageTemplate pt1) {
+        this.pt1 = pt1;
+    }
+
+    public RichPageTemplate getPt1() {
+        return pt1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+}
